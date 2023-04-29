7bf09572257805de5e2b2bff190395df6a350c57
ArmandoIsaacParraCruz <armandoisaac.parracruz@gmail.com>
2023-04-28 20:58:45 -0600

    Se agregó cambiar setpoints en el tiempo.

--- lib/ControlDeMotores/ControlDeMotores.cpp
+++ lib/ControlDeMotores/ControlDeMotores.cpp
@@ -1,14 +1,14 @@
 #include "ControlDeMotores.h"
 
 struct Motor motores[CANT_MOTORES];
-
+//EL SIGUIENTE BLOQUE DE CÓDIGO 
 PID PIDsParaMotores[CANT_MOTORES] = {
-    PID(&motores[motor1].rpmFiltrado,&motores[motor1].cicloDeTrabajo, &motores[motor1].setPointVelocidad, constantesKp[motor1], constanteski[motor1], constantesKd[motor1], DIRECT),
-    PID(&motores[motor2].rpmFiltrado,&motores[motor2].cicloDeTrabajo, &motores[motor2].setPointVelocidad, constantesKp[motor2], constanteski[motor2], constantesKd[motor2], DIRECT),
-    PID(&motores[motor3].rpmFiltrado,&motores[motor3].cicloDeTrabajo, &motores[motor3].setPointVelocidad, constantesKp[motor3], constanteski[motor3], constantesKd[motor3], DIRECT),
-    PID(&motores[motor4].rpmFiltrado,&motores[motor4].cicloDeTrabajo, &motores[motor4].setPointVelocidad, constantesKp[motor4], constanteski[motor4], constantesKd[motor4], DIRECT),
-    PID(&motores[motor5].rpmFiltrado,&motores[motor5].cicloDeTrabajo, &motores[motor5].setPointVelocidad, constantesKp[motor5], constanteski[motor5], constantesKd[motor5], DIRECT),
-    PID(&motores[motor6].rpmFiltrado,&motores[motor6].cicloDeTrabajo, &motores[motor6].setPointVelocidad, constantesKp[motor6], constanteski[motor6], constantesKd[motor6], DIRECT)
+    PID(&motores[motor1].rpmFiltrado,&motores[motor1].cicloDeTrabajo, &motores[motor1].setpointActual, constantesKp[motor1], constanteski[motor1], constantesKd[motor1], DIRECT),
+    PID(&motores[motor2].rpmFiltrado,&motores[motor2].cicloDeTrabajo, &motores[motor2].setpointActual, constantesKp[motor2], constanteski[motor2], constantesKd[motor2], DIRECT),
+    PID(&motores[motor3].rpmFiltrado,&motores[motor3].cicloDeTrabajo, &motores[motor3].setpointActual, constantesKp[motor3], constanteski[motor3], constantesKd[motor3], DIRECT),
+    PID(&motores[motor4].rpmFiltrado,&motores[motor4].cicloDeTrabajo, &motores[motor4].setpointActual, constantesKp[motor4], constanteski[motor4], constantesKd[motor4], DIRECT),
+    PID(&motores[motor5].rpmFiltrado,&motores[motor5].cicloDeTrabajo, &motores[motor5].setpointActual, constantesKp[motor5], constanteski[motor5], constantesKd[motor5], DIRECT),
+    PID(&motores[motor6].rpmFiltrado,&motores[motor6].cicloDeTrabajo, &motores[motor6].setpointActual, constantesKp[motor6], constanteski[motor6], constantesKd[motor6], DIRECT)
 };
 
 void IRAM_ATTR interrupcionSensorHall1()
@@ -46,38 +46,83 @@ void (*interrupcionesSensorHall[CANT_MOTORES])() = {
     interrupcionSensorHall6
 };
 
+void activaPWMDelMotorI(uint8_t i)
+{
+    pinMode(pinMotores[i], OUTPUT);
+    ledcSetup(i, FRECUENCIA_PWM, RESOLUCION_PWM);
+    ledcAttachPin(pinMotores[i], i);
+}
+
+void activaInterrupcionExternaDelMotorI(uint8_t i)
+{
+    pinMode(pinSensoresHall[i], INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(pinSensoresHall[i]), interrupcionesSensorHall[i], RISING);
+}
+
+void inicializaValoresDelMotorI(uint8_t i){
+    motores[i].revoluciones = VALOR_INICIAL;
+    motores[i].rpm = VALOR_INICIAL;
+    motores[i].rpmFiltrado = VALOR_INICIAL;
+    motores[i].setpointActual = motores[i].setpoints.front();
+    motores[i].setpoints.pop();
+    motores[i].minutosParaMantenerSetpointActual = motores[i].minutosParaMantenerSetpoints.front();
+    motores[i].minutosParaMantenerSetpoints.pop();
+    motores[i].tiempoAnterior = micros();
+}
+
+void activaPIDDelMotorI(uint8_t i)
+{
+    PIDsParaMotores[i].SetMode(AUTOMATIC);
+    PIDsParaMotores[i].SetOutputLimits(VALOR_INICIAL, VALOR_LIMITE_PWM);
+}
+
+void activaTimerParaActualizarLosPIDs(){
+    esp_timer_handle_t temporizadorPIDs;
+    esp_timer_create_args_t argumentosTemporizadorPIDs = {
+        .callback = &actualizaPIDs, 
+        .arg = NULL,
+        .dispatch_method = ESP_TIMER_TASK
+    };
+    esp_timer_create(&argumentosTemporizadorPIDs, &temporizadorPIDs);
+    esp_timer_start_periodic(temporizadorPIDs, PERIODO_DE_MUESTREO);
+}
+
+void activaTimerParaActualizarLosSetpoints()
+{
+    esp_timer_handle_t temporizadorSetpoint;
+    esp_timer_create_args_t argumentosTemporizadorSetpoint = {
+        .callback = &actualizaSetPoints,
+        .arg = NULL,
+        .dispatch_method = ESP_TIMER_TASK
+    };
+    esp_timer_create(&argumentosTemporizadorSetpoint, &temporizadorSetpoint);
+    esp_timer_start_periodic(temporizadorSetpoint, TIEMPO_PARA_ACTUALIZAR_SETPOINT);
+}
+
 void configurarMotores(struct Motor *configDePrueba)
 {
     bool realizaUnaVez = false;
     for(uint8_t i = 0; i < CANT_MOTORES; i++){
         motores[i].activado = configDePrueba[i].activado;
         if(motores[i].activado){
-            pinMode(pinMotores[i], OUTPUT);
-            ledcSetup(i, FRECUENCIA_PWM, RESOLUCION_PWM);
-            ledcAttachPin(pinMotores[i], i);
+            
+            activaPWMDelMotorI(i);
+            activaInterrupcionExternaDelMotorI(i);
+            
+            while(!configDePrueba[i].setpoints.empty() && !configDePrueba[i].minutosParaMantenerSetpoints.empty()){
+                motores[i].setpoints.push(configDePrueba[i].setpoints.front());
+                configDePrueba[i].setpoints.pop();
+                motores[i].minutosParaMantenerSetpoints.push(configDePrueba[i].minutosParaMantenerSetpoints.front());
+                configDePrueba[i].minutosParaMantenerSetpoints.pop();
+            }
 
-            pinMode(pinSensoresHall[i], INPUT_PULLUP);
-            attachInterrupt(digitalPinToInterrupt(pinSensoresHall[i]), interrupcionesSensorHall[i], RISING);
+            inicializaValoresDelMotorI(i);
 
-            motores[i].revoluciones = VALOR_INICIAL;
-            motores[i].rpm = VALOR_INICIAL;
-            motores[i].rpmFiltrado = VALOR_INICIAL;
-            motores[i].setPointVelocidad = configDePrueba[i].setPointVelocidad;
-            motores[i].tiempoAnterior = micros();
-
-            PIDsParaMotores[i].SetMode(AUTOMATIC);
-            PIDsParaMotores[i].SetOutputLimits(VALOR_INICIAL, VALOR_LIMITE_PWM);
-
+            activaPIDDelMotorI(i);
 
             if(!realizaUnaVez){
-                esp_timer_handle_t temporizador;
-                esp_timer_create_args_t argumentosTemporizador = {
-                    .callback = &actualizaPIDs, 
-                    .arg = NULL,
-                    .dispatch_method = ESP_TIMER_TASK
-                };
-                esp_timer_create(&argumentosTemporizador, &temporizador);
-                esp_timer_start_periodic(temporizador, PERIODO_DE_MUESTREO);
+                activaTimerParaActualizarLosPIDs();
+                activaTimerParaActualizarLosSetpoints();
             }
         }
         else{
@@ -109,7 +154,7 @@ double obtenerVelocidad(uint8_t numMotor){
     return motores[numMotor].rpmFiltrado;
 }
 double obtenerSetPointVelocidad(uint8_t numMotor){
-    return motores[numMotor].setPointVelocidad;
+    return motores[numMotor].setpointActual;
 }
 
 
@@ -120,12 +165,12 @@ void asignaCicloDeTrabajo(uint8_t numMotor,double cicloDeTrabajo){
     } 
 }
 
-void actualizaPIDs(void *arg){
+void IRAM_ATTR actualizaPIDs(void *arg){
     for(uint8_t i = 0; i < CANT_MOTORES; i++){
         if(motores[i].activado){
             calculaRPM(i);
             PIDsParaMotores[i].Compute();
-            asignaCicloDeTrabajo(i, motores[i].cicloDeTrabajo);
+            ledcWrite(i, motores[i].cicloDeTrabajo);
         }   
     }
 }
@@ -136,3 +181,28 @@ void calculaRPM(uint8_t numMotor){
     motores[numMotor].revoluciones = VALOR_INICIAL;
 }
 
+void IRAM_ATTR actualizaSetPoints(void *arg)
+{
+    for(uint8_t i = 0; i < CANT_MOTORES; i++){
+        if(motores[i].activado){
+             //Serial.println(motores[i].minutosParaMantenerSetpoints.size());
+          //double minutosTranscurridos = millis()/(1000.0*60.0); 
+            double minutosTranscurridos = millis()/(1000.0*10.0); 
+            if(minutosTranscurridos >= motores[i].minutosParaMantenerSetpointActual){
+                if(!motores[i].minutosParaMantenerSetpoints.empty() && !motores[i].setpoints.empty()){
+                    motores[i].minutosParaMantenerSetpointActual = motores[i].minutosParaMantenerSetpoints.front();
+                    motores[i].minutosParaMantenerSetpoints.pop();
+                    motores[i].setpointActual = motores[i].setpoints.front();
+                    motores[i].setpoints.pop();
+                }else{
+                    PIDsParaMotores[i].SetMode(MANUAL);
+                    motores[i].cicloDeTrabajo = VALOR_INICIAL;
+                    asignaCicloDeTrabajo(i,motores[i].cicloDeTrabajo);
+                    motores[i].setpointActual = VALOR_INICIAL;
+                    motores[i].rpmFiltrado = VALOR_INICIAL;
+                    motores[i].activado = false;
+                }
+            }
+        }
+    }
+}
